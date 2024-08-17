7695eb81ca4bdfc11a3cdb1e18b529ed6f92c5fb
Quinn Cassella <sqcassella@gmail.com>
2024-08-16 20:54:05 -0700

    Fixed and added testing

--- Problem6.py
+++ Problem6.py
@@ -1,7 +1,7 @@
 def string_oder_int(inpt):
     #please make some code that tests if the input is a integer or an string
     return -1
-def len0x(intg):
+def len0x(inpt):
     # please make some code that tests if the input is a ingteger.
     # if it isn't return -1 other wise return the len of a binary version of that int.
     return -1
@@ -10,3 +10,16 @@ def len0xOrder(int1, int2):
     # and will return negative one if either are not 
     # then will return the larger of the two
     return -1
+
+if __name__ == "__main__":
+    import sys
+    print(string_oder_int(4.1))
+    print(string_oder_int(4))
+    print(len0x(10))
+    print(len0x('10'))
+    print(len0xOrder(1,3))
+    print(len0xOrder('2',3))
+    if len(sys.argv) == 3:
+        print(string_oder_int(sys.argv[0]))
+        print(len0x(sys.argv[1]))
+        print(len0xOrder(sys.argv[2]))
