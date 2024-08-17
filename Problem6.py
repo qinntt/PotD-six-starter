35cf6834024c1befb2f6b39ceda2e105dc6a1af8
Quinn Cassella <sqcassella@gmail.com>
2024-08-16 20:40:32 -0700

    Welcome

---
+++ Problem6.py
@@ -0,0 +1,12 @@
+def string_oder_int(inpt):
+    #please make some code that tests if the input is a integer or an string
+    return -1
+def len0x(intg):
+    # please make some code that tests if the input is a ingteger.
+    # if it isn't return -1 other wise return the len of a binary version of that int.
+    return -1
+def len0xOrder(int1, int2):
+    # please make code that tests if both inputs are an integer
+    # and will return negative one if either are not 
+    # then will return the larger of the two
+    return -1
