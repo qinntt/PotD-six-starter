bb98bb15d9343b8de9b49d0442e3dc6c9ac02d92
Quinn Cassella <sqcassella@gmail.com>
2024-08-16 22:02:44 -0700

    Ok maybe be fixed

--- Problem6.py
+++ Problem6.py
@@ -1,5 +1,5 @@
 def string_oder_int(inpt):
-    #please make some code that tests if the input is a integer or an string
+    #please make some code that returns true if the input is a integer or a string if neither return -1
     return -1
 def len0x(inpt):
     # please make some code that tests if the input is a ingteger.
@@ -11,9 +11,23 @@ def len0xOrder(int1, int2):
     # then will return the larger of the two
     return -1
 
+def treat_input(inpt):
+        try:
+            if str(inpt).count('.') == 1:
+                return float(inpt)
+            else:
+                return int(inpt)
+        except:
+            return str(inpt)
+
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) == 3:
-        print(string_oder_int(sys.argv[0]))
-        print(len0x(sys.argv[1]))
-        print(len0xOrder(sys.argv[2]))
+    if len(sys.argv) == 5:
+        holder  = treat_input(sys.argv[1])
+        holder1 = treat_input(sys.argv[2])
+        holder2 = treat_input(sys.argv[3])
+        holder3 = treat_input(sys.argv[4])
+
+        print(string_oder_int(holder))
+        print(len0x(holder1))
+        print(len0xOrder(holder2, holder3))
